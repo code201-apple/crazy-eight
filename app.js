78ec3cf4979f0d357efbd76707732c382305191d
Charlie Heiner <heinercj@gmail.com>
2017-11-16 14:50:40 -0800

    took out generic answers that caused glitch

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-const answersArray = ['yes', 'It is certain', 'It is decidedly so', 'Without a doubt', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Outlook good', 'Signs point to yes', 'Reply is hazy, try again', 'Ask again later', 'Better not tell you now', 'Cannot predict now', 'Concentrate and try again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Outlook is not so good', 'Very doubtful'];
+const answersArray = ['Yes', 'It is certain', 'Yup', 'Absolutely', 'Yes definitely', 'You may rely on it', 'As I see it, yes', 'Most likely', 'Of course', 'Signs point to yes', 'Reply is hazy, try again', 'Cannot predict now', 'Please try again', 'Don\'t count on it', 'My reply is no', 'My sources say no', 'Not looking good', 'Very doubtful'];
 const userAnswers = [];
 
 const eightBall = new FortuneTell('eightBall');
